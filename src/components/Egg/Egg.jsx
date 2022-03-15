97dc24564cab11d846cba5c63083ca7aeb692ea3
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-14 21:21:09 -0500

    trying to add animations to the egg

--- src/components/Egg/Egg.jsx
+++ src/components/Egg/Egg.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import './Egg.css'
+
 export default function Egg() {
     const egg = useSelector(store => store.egg);
     const eggUrl = `/images/eggs/egg-${egg.egg_id}-${egg.status}.png`
