bfb3356b4974b5befe62f0efc0b9f9bfdf29a125
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-15 16:21:24 -0500

    background added to flockpage

--- src/components/FlockPage/FlockPage.jsx
+++ src/components/FlockPage/FlockPage.jsx
@@ -1,6 +1,9 @@
+import './FlockPage.css'
+
 export default function FlockPage() {
     return(
-        <>
-        </>
+        <div className="FlockPage">
+            <h2>FLOCK PAGE</h2>
+        </div>
     )
 }
