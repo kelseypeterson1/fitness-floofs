e69bfe9b29ec1474a404d2b7c7ae459d4c17a0b1
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-16 10:38:57 -0500

    profile route set up and working

--- src/components/FloofProfile/FloofProfile.jsx
+++ src/components/FloofProfile/FloofProfile.jsx
@@ -1,6 +1,32 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import Button from '@mui/material/Button';
+import { useHistory } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 export default function FloofProfile() {
-    return(
+
+    const history = useHistory();
+    const { id } = useParams();
+    const flock = useSelector(store => store.flock);
+    const floof = flock[id];
+    const dispatch = useDispatch();
+    const user = useSelector((store) => store.user);
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_FLOCK', payload: user });
+    }, []);
+
+    const handleClick = () => {
+        history.push(`/flock`);
+    }
+
+    return (
         <>
+            <h2>{floof.name}</h2>
         </>
     )
 }
