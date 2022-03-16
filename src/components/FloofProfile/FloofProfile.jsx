88a7b4a5c65c623d1232bf57cefd21297a82b3da
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-16 11:13:22 -0500

    correct floof rendering on dom

--- src/components/FloofProfile/FloofProfile.jsx
+++ src/components/FloofProfile/FloofProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
@@ -12,12 +12,21 @@ export default function FloofProfile() {
     const history = useHistory();
     const { id } = useParams();
     const flock = useSelector(store => store.flock);
-    const floof = flock[id];
+    const floof = flock[id-1];
     const dispatch = useDispatch();
     const user = useSelector((store) => store.user);
+    const imageUrl = `images/floofs/floof${floof.floof_id}.png`
+    console.log('floof is', floof)
+    console.log('params is', id)
+    console.log('id is', floof.floof_id)
+
+    // let [floofy, setFloofy] = useState([])
 
     useEffect(() => {
+        // console.log('in useEffect')
         dispatch({ type: 'FETCH_FLOCK', payload: user });
+        // setFloofy(floof);
+        // console.log('floofy is', floofy)
     }, []);
 
     const handleClick = () => {
@@ -27,6 +36,7 @@ export default function FloofProfile() {
     return (
         <>
             <h2>{floof.name}</h2>
+            <img src={imageUrl} />
         </>
     )
 }
