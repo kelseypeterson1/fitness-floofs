d373352ad7aa27ce6c76784f41127a1e3bf15290
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-16 09:31:03 -0500

    cleaned up component folders

--- src/index.js
+++ src/index.js
@@ -6,19 +6,19 @@ import store from './redux/store';
 
 import App from './components/App/App';
 
-export { default as AboutPage } from './components/AboutPage/AboutPage'
+export { default as AboutPage } from './components/TemplateFunction/AboutPage/AboutPage'
 export { default as App } from './components/App/App'
 export { default as Footer } from './components/Footer/Footer'
 export { default as Header } from './components/Header/Header'
 export { default as Homepage } from './components/Homepage/Homepage'
-export { default as InfoPage } from './components/InfoPage/InfoPage'
+export { default as InfoPage } from './components/TemplateFunction/InfoPage/InfoPage'
 export { default as LandingPage } from './components/LandingPage/LandingPage'
-export { default as LoginForm } from './components/LoginForm/LoginForm'
+export { default as LoginForm } from './components/LoginPage/LoginForm/LoginForm'
 export { default as LoginPage } from './components/LoginPage/LoginPage'
 export { default as LogOutButton } from './components/Nav/LogOutButton/LogOutButton'
 export { default as Nav } from './components/Nav/Nav'
 export { default as ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute'
-export { default as RegisterForm } from './components/RegisterForm/RegisterForm'
+export { default as RegisterForm } from './components/RegisterPage/RegisterForm/RegisterForm'
 export { default as RegisterPage } from './components/RegisterPage/RegisterPage'
 export { default as Egg } from './components/Homepage/Egg/Egg'
 export { default as StepCounter } from './components/Homepage/StepCounter/StepCounter'
