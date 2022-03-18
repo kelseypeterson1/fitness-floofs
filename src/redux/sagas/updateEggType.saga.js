6a26571ef990dffe1bfea0f11581220d0b7d7069
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-18 11:59:25 -0500

    new egg is added based on probability

--- src/redux/sagas/updateEggType.saga.js
+++ src/redux/sagas/updateEggType.saga.js
@@ -3,13 +3,19 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* updateEggType(action) {
     console.log('in updateEggTypeSaga');
-    yield console.log('in updateEggTypeSaga2');
-    const newEggId = 3;
 
+    // generating new random egg for user
+    const randomWithProbability = () => {
+        const notRandomNumbers = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 11];
+        const idx = Math.floor(Math.random() * notRandomNumbers.length);
+        return notRandomNumbers[idx];
+      }
+      const newEggId = randomWithProbability();
+      console.log('new randomly generated egg is', newEggId)
+
+    // sending the new egg to the server
     try {
         yield axios.put(`/egg/${action.payload.user_id}`, { date: action.payload.date, newEgg: newEggId })
-        // yield put({ type: 'FETCH_FLOCK', payload: action.payload.user_id });
-        // yield put({ type: 'FETCH_SELECTED_FLOOF', payload: action.payload.id })
     } catch {
         yield console.log('PUT change egg type client-side error');
     }
