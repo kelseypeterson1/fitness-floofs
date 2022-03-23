db7312c7a1c0a0020a3562a2835f477c2e673788
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-23 14:49:23 -0500

    moved floofs upward to make room for the nav bar

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ export { default as InfoPage } from './components/TemplateFunction/InfoPage/Info
 export { default as LandingPage } from './components/LandingPage/LandingPage'
 export { default as LoginForm } from './components/LoginPage/LoginForm/LoginForm'
 export { default as LoginPage } from './components/LoginPage/LoginPage'
-export { default as LogOutButton } from './components/Nav/LogOutButton/LogOutButton'
 export { default as Nav } from './components/Nav/Nav'
 export { default as ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute'
 export { default as RegisterForm } from './components/RegisterPage/RegisterForm/RegisterForm'
