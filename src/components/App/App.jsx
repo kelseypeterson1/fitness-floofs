0292b3a6e1066e8d87a8533c309d0d0a130bdc86
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-03-24 22:44:14 -0500

    styled eggHatchConflictAlert

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,15 +27,6 @@ function App() {
           {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
           <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-            <AboutPage />
-          </Route>
-
           {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the Homepage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
@@ -54,14 +45,6 @@ function App() {
             path="/homepage"
           >
             <Homepage />
-          </ProtectedRoute>
-
-          <ProtectedRoute
-            // logged in shows InfoPage else shows LoginPage
-            exact
-            path="/info"
-          >
-            <InfoPage />
           </ProtectedRoute>
 
           <ProtectedRoute
